01941aa16e4f9743d8e18e9b17c5193cc6631a62
pawelk12 <pawel.krempa12@gmail.com>
2024-12-24 23:42:30 +0100

    feat: add @nestjs/config to package.json and app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ListingsModule } from './modules/listings/listings.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ListingsModule, PrismaModule],
+  imports: [
+    ListingsModule,
+    PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
