a51dbba288c898b1a82bc2bdadce0f65b8ab9ca5
pawelk12 <pawel.krempa12@gmail.com>
2024-12-31 11:25:14 +0100

    feat(auth): add TokenModule and TokenService for JWT handling

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { ListingsModule } from './modules/listings/listings.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { TokenModule } from './modules/token/token.module';
 
 @Module({
   imports: [
     ListingsModule,
     PrismaModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    TokenModule,
   ],
   controllers: [AppController],
   providers: [AppService],
