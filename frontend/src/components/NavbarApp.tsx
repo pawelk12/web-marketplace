db23fd7c578d8a27226556c814cc31ec4fce09fe
pawelk12 <pawel.krempa12@gmail.com>
2025-01-14 23:41:49 +0100

    feat(logout ui): add log out button

--- frontend/src/components/NavbarApp.tsx
+++ frontend/src/components/NavbarApp.tsx
@@ -1,6 +1,8 @@
 import {IconBuildingStore, IconCirclePlus} from "@tabler/icons-react";
-import {Box, NavLink} from "@mantine/core";
+import {Box, Button, NavLink} from "@mantine/core";
 import {useLocation, useNavigate} from "react-router-dom";
+import {API_URL} from "../config.ts";
+import {ErrorNotification} from "../features/notifications/notifications.ts";
 
 const data = [
     {icon: IconBuildingStore, label: 'Marketplace', link: '/listings'},
@@ -12,6 +14,23 @@ export const NavbarApp = () => {
 
     const navigate = useNavigate();
     const location = useLocation();
+
+    const handleLogOut = async () => {
+        try {
+            const response = await fetch(`${API_URL}/auth/logout`, {
+                method: "POST",
+                credentials: "include",
+            })
+
+            if (!response.ok) {
+                throw new Error("Something went wrong");
+            }
+            navigate("/login");
+        }catch(e){
+            const err = e as Error;
+            ErrorNotification(err.message);
+        }
+    }
 
     const items = data.map((item) => (
         <NavLink
@@ -28,5 +47,19 @@ export const NavbarApp = () => {
         />
     ));
 
-    return <Box w={265}>{items}</Box>;
+    return (
+        <>
+            <Box w={265} style={{height: '100vh'}}>{items}</Box>
+            <Button w={265} h = {50} style={{ marginBottom: '0.5rem',
+                backgroundColor: 'rgba(236,63,63,0.18)',
+                color: 'rgb(243,214,214)',
+                fontSize: '0.8rem',}}
+                onClick={handleLogOut}>
+
+                Log Out
+            </Button>
+        </>
+
+
+    );
 }
